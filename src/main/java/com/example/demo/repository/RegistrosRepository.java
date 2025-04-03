c2438211e29d9e87314030e6d1ce9057068ace27
Miguel32-lang <miguelmartinezg2005@gmail.com>
2025-04-03 05:21:09 -0600

    Update RegistrosRepository.java

--- src/main/java/com/example/demo/repository/RegistrosRepository.java
+++ src/main/java/com/example/demo/repository/RegistrosRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Registros;
 
 public interface RegistrosRepository extends JpaRepository<Registros, Long>{
-  Registros findTopByOrderByIdDesc();
 }
