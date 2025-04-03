8058d75f35e9fc971d1a85a5573ba971f64ecd9c
Miguel32-lang <miguelmartinezg2005@gmail.com>
2025-04-03 05:17:07 -0600

    Update BodegaApplication.java

--- src/main/java/com/example/demo/BodegaApplication.java
+++ src/main/java/com/example/demo/BodegaApplication.java
@@ -8,6 +8,11 @@ public class BodegaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BodegaApplication.class, args);
+		registry.addMapping("/")
+						.allowedOrigins("http://127.0.0.1:5500","http://localhost:8080","http://localhost:8100")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
 	}
 
 }
