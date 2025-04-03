2f5bfabf2ada0db7b2d867dbb7d39c367fe256e7
Miguel32-lang <miguelmartinezg2005@gmail.com>
2025-04-03 05:19:49 -0600

    Update BodegaApplication.java

--- src/main/java/com/example/demo/BodegaApplication.java
+++ src/main/java/com/example/demo/BodegaApplication.java
@@ -1,18 +1,31 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BodegaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BodegaApplication.class, args);
-		registry.addMapping("/")
+		
+	}
+
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/")
 						.allowedOrigins("http://127.0.0.1:5500","http://localhost:8080","http://localhost:8100")
 						.allowedMethods("*")
 						.allowedHeaders("*")
 						.allowCredentials(true);
+			}
+		};
 	}
 
 }
