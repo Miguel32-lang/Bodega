ef38125b57993799869d8965ec1c12d9f30d5abe
Miguel32-lang <miguelmartinezg2005@gmail.com>
2025-04-07 16:32:55 -0600

    Update BodegaApplication.java

--- src/main/java/com/example/demo/BodegaApplication.java
+++ src/main/java/com/example/demo/BodegaApplication.java
@@ -20,7 +20,7 @@ public class BodegaApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://127.0.0.1:5500","http://localhost:8080","http://localhost:8100","http://localhost")
+						.allowedOrigins("http://127.0.0.1:5500","http://localhost:8080","http://localhost:8100","https://localhost")
 						.allowedMethods("*")
 						.allowedHeaders("*")
 						.allowCredentials(true);
